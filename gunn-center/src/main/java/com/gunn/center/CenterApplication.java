2a025a64db9e3717710669c5013c73b6a10ba211
ganjunhui <junhui_g@gzleihou.cn>
2020-01-04 21:51:18 +0800

    add center modules

--- gunn-center/src/main/java/com/gunn/center/CenterApplication.java
+++ gunn-center/src/main/java/com/gunn/center/CenterApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class CenterApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(CenterApplication.class, args);
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(CenterApplication.class, args);
+	}
 
 }
